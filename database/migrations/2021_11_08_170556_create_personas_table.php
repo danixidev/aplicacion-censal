fb1fb665c517a2a5e81045028ee5b1c210b269d5
Dani <danixidev@gmail.com>
2021-11-08 19:36:01 +0100

    Update 2021_11_08_170556_create_personas_table.php

--- database/migrations/2021_11_08_170556_create_personas_table.php
+++ database/migrations/2021_11_08_170556_create_personas_table.php
@@ -19,9 +19,6 @@ class CreatePersonasTable extends Migration
             $table->string("primer_apellido", 50);
             $table->string("segundo_apellido", 50);
             $table->date("nacimiento");
-
-            $table->foreignId('domicilio_id')->constrained();
-
             $table->timestamps();
         });
     }
