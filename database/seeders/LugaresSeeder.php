dacc8d84ce97f6904d1c02ebf302b2ac7a0765e9
Dani <danixidev@gmail.com>
2021-11-17 19:03:01 +0100

    Added 'real' names to make the database easier to understand

--- database/seeders/LugaresSeeder.php
+++ database/seeders/LugaresSeeder.php
@@ -16,16 +16,22 @@ class LugaresSeeder extends Seeder
      */
     public function run()
     {
+        $comunidades = ["Andalucía", "Aragón", "Canarias", "Cantabria", "Castilla y León", "Castilla-La Mancha", "Cataluña", "Ceuta", "Comunidad Valenciana", "Comunidad de Madrid", "Extremadura", "Galicia", "Islas Baleares", "La Rioja", "Melilla", "Navarra", "País Vasco", "Principado de Asturias", "Región de Murcia"];
+        $provincias = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara','Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
+        $localidades = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara','Guipúzcoa','Huelva','Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya','Zamora','Zaragoza'];
+
+
         for ($i=1; $i <= 10; $i++) {
+
             DB::table('comunidades')->insert([
-                'nombre_comunidad' => Str::random(10),
+                'nombre_comunidad' => $comunidades[$i],
             ]);
             DB::table('provincias')->insert([
-                'nombre_provincia' => Str::random(10),
+                'nombre_provincia' => $provincias[$i],
                 'comunidad_id' => $i,
             ]);
             DB::table('localidades')->insert([
-                'nombre_localidad' => Str::random(10),
+                'nombre_localidad' => $localidades[$i+10],
                 'provincia_id' => $i,
             ]);
             DB::table('cps')->insert([
